b9faa1a83f5a4895e378fb9376dddd60d32eb0de
zachfranzen123 <hx9nj9nfmr@privaterelay.appleid.com>
2026-07-16 11:42:18 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="cabin-drinks-v16";
-const SHELL=["./","./index.html","./app.html","./landing.css","./history.css","./landing.js","./style.css","./multi-order.css","./help.css","./v7.css","./v8.css","./delivery-details.css","./v10.css","./food.css","./app.js","./manifest.json","./app-icon.svg"];
+const CACHE="cabin-drinks-v17";
+const SHELL=["./","./index.html","./app.html","./landing.css","./history.css","./about.css","./landing.js","./style.css","./multi-order.css","./help.css","./v7.css","./v8.css","./delivery-details.css","./v10.css","./food.css","./app.js","./manifest.json","./app-icon.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match("./app.html"))))});
