bc86f35ed2a6c22fea1dc69ce4e6097b0478f10c
zachfranzen123 <hx9nj9nfmr@privaterelay.appleid.com>
2026-07-22 18:03:34 -0700

    Refresh cache for condensed version history

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="cabin-drinks-v14-readiness-fix";
+const CACHE="cabin-drinks-v14-history-collapse";
 const SHELL=["./","./index.html","./app.html","./landing.css","./history.css","./about.css","./support.css","./showcase.css","./hero-mockup.css","./install.css","./landing.js","./updater.js","./style.css","./multi-order.css","./help.css","./v7.css","./v8.css","./delivery-details.css","./v10.css","./food.css","./usability.css","./v13.css","./app.js","./manifest.json","./app-icon.svg","./app-seat-map.png","./app-prepare-orders.png","./A1C049B2-7FF4-41EC-9220-E4BF0FF22D71.png"];
 
 self.addEventListener("install",event=>{
