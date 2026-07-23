01a2d0a07f637214c7f593489c76cef74d81d951
zachfranzen123 <hx9nj9nfmr@privaterelay.appleid.com>
2026-07-22 17:43:51 -0700

    Refresh cache for circular portrait fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="cabin-drinks-v14-about-elio";
+const CACHE="cabin-drinks-v14-about-circle";
 const SHELL=["./","./index.html","./app.html","./landing.css","./history.css","./about.css","./support.css","./showcase.css","./hero-mockup.css","./install.css","./landing.js","./updater.js","./style.css","./multi-order.css","./help.css","./v7.css","./v8.css","./delivery-details.css","./v10.css","./food.css","./usability.css","./v13.css","./app.js","./manifest.json","./app-icon.svg","./app-seat-map.png","./app-prepare-orders.png","./A1C049B2-7FF4-41EC-9220-E4BF0FF22D71.png"];
 
 self.addEventListener("install",event=>{
